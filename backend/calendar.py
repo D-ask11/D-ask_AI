2e7da8d72e86ed7334ae4c00b355617ed6988a7d
지성환 <jsh91252@gmail.com>
2025-12-19 19:20:17 +0900

    Update calendar.py

--- backend/calendar.py
+++ backend/calendar.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from models import CalendarRequest, CalendarItem
 from database import database
 from datetime import datetime
 
-app = FastAPI()
+app = FastAPI()  
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST"],
+    allow_headers=["Content-Type"],
+)
 
 @app.post("/calendar", response_model=list[CalendarItem])
 def get_calendar(req: CalendarRequest):
     year, month = req.year, req.month
     today = datetime.today()
 
-    # 예외 처리 1: 너무 과거 or 미래(404)
     if (year, month) not in database:
         raise HTTPException(status_code=404, detail="아직 오지 않았거나 찾을 수 없는 달입니다.")
 
-    # 데이터 반환
     return database[(year, month)]
