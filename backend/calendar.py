ed4947086bc80433fe2776bd0a0003d70eccd0de
지성환 <jsh91252@gmail.com>
2025-12-19 19:22:40 +0900

    Update calendar.py

--- backend/calendar.py
+++ backend/calendar.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:5501/calenderPage/index.html"],
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
 )
