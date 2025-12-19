fa6dfa4caf3bdf63566bfc86d0dfed61141095a8
hawon223 <hwyw0223@gmail.com>
2025-12-19 19:31:38 +0900

    add _middleware

--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ai.model import rag_chain, rag_inference
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/", tags=["main"])
 async def root():
     return {"hello":"wolrd"}
